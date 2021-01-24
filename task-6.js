cc267e4290c68662bc19eddc0a68b654f02f1730
Mykyta1 <whitelion1499@gmail.com>
2021-01-24 23:30:58 +0200

    push

---
+++ task-6.js
@@ -0,0 +1,15 @@
+let input;
+const numbers = [];
+let total = 0;
+input = prompt("Введите число!");
+for (let i = 0; input !== null; i++) {
+    if (isNaN(input)){
+    alert("Было введено не число, попробуйте еще раз");
+    input = prompt("Введите число!");}
+    else  {
+    let number = Number.parseInt(input);
+    numbers.push(number);
+    input = prompt("Введите число!");}
+  }
+  for (let i = 0, sum = 0; i < numbers.length; sum += numbers[i++])
+  alert(`Общая сумма чисел равна ${sum}`);
